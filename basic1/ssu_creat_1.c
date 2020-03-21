f59b03493cc763b1a20431d82d8851cd6ceefcdd
shlee4290 <shlee4290@naver.com>
2020-03-21 22:45:19 +0900

    basic1 주석 추가

--- basic1/ssu_creat_1.c
+++ basic1/ssu_creat_1.c
@@ -4,15 +4,17 @@
 #include <fcntl.h>
 
 int main(){
-	char *fname = "ssu_test.txt";
-	int fd;
+	char *fname = "ssu_test.txt"; // creat할 파일 이름
+	int fd; // 파일 디스크립터 저장할 변수
 
-	if((fd = creat(fname, 0666)) < 0){
+	if((fd = creat(fname, 0666)) < 0){ // creat가 실패했다면
+		// stderr에 에러메세지 출력
 		fprintf(stderr, "creat error for %s\n", fname);
-		exit(1);
-	} else {
+		exit(1); // 프로그램 종료
+	} else { // creat 성공했다면
+		// 성공 메세지 출력
 		printf("Success!\nFilename : %s\nDescriptor : %d\n", fname, fd);
-		close(fd);
+		close(fd); // 파일 close
 	}
 
 	exit(0);
