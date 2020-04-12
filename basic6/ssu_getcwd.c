2f7164ccc5ed1eb65407b86d9d324824f551099b
shlee4290 <shlee4290@naver.com>
2020-04-12 17:39:00 +0900

    basic6 complete

---
+++ basic6/ssu_getcwd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define PATH_MAX 1024
+
+int main(void)
+{
+	char *pathname;
+
+	if (chdir("/home/oslab") < 0) {
+		fprintf(stderr, "chdir error\n");
+		exit(1);
+	}
+
+	pathname = malloc(PATH_MAX);
+
+	if (getcwd(pathname, PATH_MAX) == NULL) {
+		fprintf(stderr, "getcwd error\n");
+		exit(1);
+	}
+
+	printf("current directory = %s\n", pathname);
+	exit(0);
+}
