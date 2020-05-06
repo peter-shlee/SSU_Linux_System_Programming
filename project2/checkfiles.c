246b0fb26d0016e2e6a3117c497709c0b7a5082c
shlee4290 <shlee4290@naver.com>
2020-05-06 15:29:47 +0900

    project2 checkfile ...

--- project2/checkfiles.c
+++ project2/checkfiles.c
@@ -21,11 +21,16 @@ struct FileData {
 	int filesCnt;
 };
 
+void checkFileDataStatus(const struct FileData *prevFileData, const struct FileData *curFileData);
+void updateFileDataStatus(struct FileData **prevFileData, struct FileData **curFileData);
 void getCurFileState(struct FileData *fileData, const char *dirname);
 void printFileData(const struct FileData *fileData);
+void sortFileData(struct FileData *fileData);
+int compareFileInfo(const void *a, const void *b);
 
 int main(void)
 {
+	char path[PATH_MAX];
 	struct FileData *prevFileData;
 	struct FileData *curFileData;
 	prevFileData = (struct FileData *) malloc(sizeof(struct FileData));
@@ -33,12 +38,45 @@ int main(void)
 	prevFileData->filesCnt = 0;
 	curFileData->filesCnt = 0;
 
-	curFileData->filesCnt = 0; // 새로운 파일 데이터 생성(getCurFileState 호출) 전에 반드시 filesCnt를 0으로 만들어 줘야 한다
-	getCurFileState(curFileData, TARGET_DIRECTORY_NAME);
-	// 이진 검색을 위해 생성된 file 데이터 sort
+//	getcwd(path, PATH_MAX);
+//
+//	curFileData->filesCnt = 0; // 새로운 파일 데이터 생성(getCurFileState 호출) 전에 반드시 filesCnt를 0으로 만들어 줘야 한다
+//	getCurFileState(curFileData, TARGET_DIRECTORY_NAME);
+//	chdir(path);
+//	// 이진 검색을 위해 생성된 file 데이터 sort
+//
+//	sortFileData(curFileData);
+//
+
+	updateFileDataStatus(&prevFileData, &curFileData);
 
 	printFileData(curFileData);
+	printFileData(prevFileData);
 
+}
+
+void checkFileDataStatus(const struct FileData *prevFileData, const struct FileData *curFileData) {
+
+
+}
+
+void updateFileDataStatus(struct FileData **prevFileData, struct FileData **curFileData) {
+	char path[PATH_MAX];
+	struct FileData **tmp;
+	getcwd(path, PATH_MAX);
+
+	// 새롭게 데이터를 갱신해야 하므로 curFileData를 prevFileData로 옮김, prevFileData는 새로운 파일 정보를 넣어 재활용
+	tmp = prevFileData;
+	prevFileData = curFileData;
+	curFileData = tmp;
+
+	(*curFileData)->filesCnt = 0; // 새로운 파일 데이터 생성(getCurFileState 호출) 전에 반드시 filesCnt를 0으로 만들어 줘야 한다
+	getCurFileState(*curFileData, TARGET_DIRECTORY_NAME);
+	sortFileData(*curFileData);
+
+	chdir(path);
+
+	return;
 }
 
 void getCurFileState(struct FileData *fileData, const char *dirname){
@@ -92,4 +130,12 @@ void printFileData(const struct FileData *fileData) {
 	}
 	printf("print file data end\n");
 	printf("\n");
+}
+
+void sortFileData(struct FileData *fileData){
+	qsort(fileData->files, fileData->filesCnt, sizeof(struct FileInfo), compareFileInfo);
+}
+
+int compareFileInfo(const void *a, const void *b) {
+	return strcmp(((const struct FileInfo *)a)->absolutePathName, ((const struct FileInfo *)b)->absolutePathName);
 }
