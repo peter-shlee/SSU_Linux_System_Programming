a5e3627d57b3f3915dc62d28b9b078592491b8cb
shlee4290 <shlee4290@naver.com>
2020-05-23 17:45:41 +0900

    basic18 complete

---
+++ basic18/ssu_fdcopy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#define MSG "message will be written to Termial\n"
+
+int main(void)
+{
+	int new_fd;
+
+	if ((new_fd = fcntl(STDOUT_FILENO, F_DUPFD, 3)) == -1) {
+		fprintf(stderr, "Error : Copying File Descriptor\n");
+		exit(1);
+	}
+
+	close(STDOUT_FILENO);
+	write(3, MSG, strlen(MSG));
+	exit(0);
+}
---
+++ basic18/ssu_fddup.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int testfd;
+	int fd;
+
+	fd = open("test.txt", O_CREAT);
+
+	testfd = fcntl(fd, F_DUPFD, 5);
+	printf("testfd : %d\n", testfd);
+	testfd = fcntl(fd, F_DUPFD, 5);
+	printf("testfd : %d\n", testfd);
+
+	getchar();
+}
